780cbb9aa5e4834e467207755bb2c9990c7b1bda
satells <msergiost@hotmail.com>
2022-10-08 18:28:20 -0300

    Criada e testada a classe "Aluno".

---
+++ cleanescola/src/main/java/cleanescola/Aluno.java
@@ -0,0 +1,90 @@
+package cleanescola;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//https://howtodoinjava.com/design-patterns/creational/builder-pattern-in-java/
+public class Aluno {
+
+	private CPF cpf;
+
+	private String nome;
+
+	private Email email;
+
+	private List<Telefone> telefones = new ArrayList<Telefone>();
+
+	private Aluno(AlunoBuilder builder) {
+
+		this.cpf = builder.cpf;
+		this.nome = builder.nome;
+		this.email = builder.email;
+
+	}
+
+	public CPF getCpf() {
+		return cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public Email getEmail() {
+		return email;
+	}
+
+	public void adicionarTelefone(String ddd, String numero) {
+		this.telefones.add(new Telefone(ddd, numero));
+	}
+
+	public static class AlunoBuilder {
+		private CPF cpf;
+
+		private String nome;
+
+		private Email email;
+
+		public AlunoBuilder() {
+		}
+
+		public AlunoBuilder comCpf(CPF cpf) {
+			this.cpf = cpf;
+			return this;
+		}
+
+		public AlunoBuilder comNome(String nome) {
+			this.nome = nome;
+			return this;
+		}
+
+		public AlunoBuilder comEmail(Email email) {
+			this.email = email;
+			return this;
+		}
+
+		public Aluno build() {
+			Aluno aluno = new Aluno(this);
+
+			validateAluno(aluno);
+
+			return aluno;
+		}
+
+		private void validateAluno(Aluno aluno) {
+			if (aluno.getNome() == null) {
+				throw new IllegalArgumentException("Nome é obrigatório.");
+			} else if (aluno.getCpf() == null) {
+				throw new IllegalArgumentException("CPF é obrigatório.");
+			} else if (aluno.getEmail() == null) {
+				throw new IllegalArgumentException("E-mail é obrigatório.");
+			}
+
+		}
+
+	}
+
+	public Telefone getTelefone(int index) {
+		return this.telefones.get(index);
+	}
+}
---
+++ cleanescola/src/test/java/cleanescola/AlunoTest.java
@@ -0,0 +1,44 @@
+package cleanescola;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AlunoTest {
+
+	@Test
+	void testar_criacao_aluno() {
+
+		CPF cpf = new CPF("08454871080");
+		Email email = new Email("email@email.com");
+		String nome = "Ricardo Mendes";
+		Aluno aluno = new Aluno.AlunoBuilder().comCpf(cpf).comEmail(email).comNome(nome).build();
+
+		String ddd = "12";
+		String numero = "123456789";
+		aluno.adicionarTelefone(ddd, numero);
+
+		assertEquals(aluno.getNome(), nome);
+		assertEquals(aluno.getCpf().getNumero(), cpf.getNumero());
+		assertEquals(aluno.getEmail().getEndereco(), email.getEndereco());
+
+		Telefone telefone = aluno.getTelefone(0);
+		assertEquals(telefone.getDdd(), ddd);
+		assertEquals(telefone.getNumero(), numero);
+
+	}
+
+	@Test
+	void testar_criacao_de_aluno_com_erro() {
+		CPF cpf = new CPF("08454871080");
+		Email email = new Email("email@email.com");
+		String nome = "Ricardo Mendes";
+
+		Assertions.assertThrows(IllegalArgumentException.class, () -> new Aluno.AlunoBuilder().comCpf(null).comEmail(email).comNome(nome).build());
+		Assertions.assertThrows(IllegalArgumentException.class, () -> new Aluno.AlunoBuilder().comCpf(cpf).comEmail(null).comNome(nome).build());
+		Assertions.assertThrows(IllegalArgumentException.class, () -> new Aluno.AlunoBuilder().comCpf(cpf).comEmail(email).comNome(null).build());
+
+	}
+
+}
