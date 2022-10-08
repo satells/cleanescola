aadc0958ae0601ce67b9a2b64916b26180c8ba23
satells <msergiost@hotmail.com>
2022-10-08 16:50:16 -0300

    Classe "Email" que está agregada à classe "Aluno" criada e restada.

---
+++ cleanescola/src/main/java/cleanescola/Email.java
@@ -0,0 +1,19 @@
+package cleanescola;
+
+public class Email {
+
+	public Email(String endereco) {
+
+		if (endereco == null || !endereco.matches("^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
+			throw new IllegalArgumentException("Endereço de E-mail está errado.");
+		}
+		this.endereco = endereco;
+	}
+
+	private String endereco;
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+}
